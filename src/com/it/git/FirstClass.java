16e0d5cda6f84e0bff209803b7e5475630ea0edc
zyq <1745552507@qq.com>
2021-02-18 21:31:00 +0800

    把本地工程添加到git

---
+++ src/com/it/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.it.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
